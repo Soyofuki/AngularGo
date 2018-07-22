da8407e8fd284990eafa5eca16ba059c02cc1426
Weifeng Chen <Soyofuki@users.noreply.github.com>
2018-07-22 11:14:00 +0800

    Implement sub navItem; Add grid layout for dashboard

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
+
+export interface NavItem {
+  name: string;
+  routerLink: string;
+  subItems: NavItem[];
+}
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  selector: "app-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.scss"]
 })
 export class LayoutComponent implements OnInit {
-
   loading = false;
 
   mobileQuery: MediaQueryList;
 
-  fillerNav = [
-    {name: "Home", routerLink: "home"},
-    {name: "Help", routerLink: "help"},
+  items: NavItem[] = [
+    {
+      name: "Home", routerLink: "home", subItems: null
+    },
+    {
+      name: "Sample", routerLink: "sample",
+      subItems: [
+        { name: "Sample1", routerLink: "sample1", subItems: null },
+        { name: "Sample2", routerLink: "sample2", subItems: null }
+      ]
+    },
+    {
+      name: "Help", routerLink: "help", subItems: null
+    }
   ];
 
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-
-
 }
--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +14,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'Layout', cols: 3, rows: 1, color: '#c2185b'},
+    {text: 'Form', cols: 1, rows: 2, color: '#c2185b'},
+    {text: 'Table', cols: 1, rows: 1, color: '#c2185b'},
+    {text: 'Popup', cols: 2, rows: 1, color: '#c2185b'},
+  ];
 
   constructor(){
 
